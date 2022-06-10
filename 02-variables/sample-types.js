090b2b37334cdfbbbb339bda0ad08b0f311798c6
Muhammad Tahir <64077988+MUHAMMADTAHIR31@users.noreply.github.com>
2022-06-11 01:54:10 +0500

    Add files via upload

---
+++ 02-variables/sample-types.js
@@ -0,0 +1,7 @@
+var found = true;
+var grade = 88.8;
+var firstName = "John";
+var lastName = "Doe";
+console.log(found);
+console.log("The grade is " + grade);
+console.log("My name is " + firstName + " " + lastName);
---
+++ 02-variables/sample-types.ts
@@ -0,0 +1,8 @@
+let found: boolean = true;
+let grade: number = 88.8;
+let firstName: string = "John";
+let lastName: string = "Doe";
+
+console.log(found);
+console.log("The grade is " + grade);
+console.log("My name is " + firstName + " " + lastName);
